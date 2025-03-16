48a9e6b1f05c54f487159f9b3f46a65996aa4f78
sirley15 <sirley15m@gmail.com>
2025-03-16 11:32:13 -0500

    Actualizando cambios

--- punto4.ts
+++ punto4.ts
@@ -1,8 +1,23 @@
-class Repositorio <T>{
-    private elementos: T [] = [];
-
-    agregar(elementos:T):void{
-        this.elementos.push(elementos);
+class Repositorio<T> {
+    private elementos: T[] = []; 
+  
+    agregar(elemento: T): void {
+      this.elementos.push(elemento); 
     }
+    obtener(indice: number): T {
+      return this.elementos[indice];
+    }
+  
+    eliminar(indice: number): void {
+      this.elementos.splice(indice, 1); 
+    }
+  }
+  const repo = new Repositorio<string>(); 
 
-}
+repo.agregar("Hola");  
+repo.agregar("Mundo"); 
+repo.agregar("TYPESCRIPT");
+
+console.log(repo.obtener(0));
+console.log(repo.obtener(2));  
+repo.eliminar(1); 
